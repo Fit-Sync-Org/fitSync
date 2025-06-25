4920ffcac14217a9cf836107e10e7a6a0d8752f1
Jason Jason <jasyn.j.64@gmail.com>
2025-06-24 17:42:20 -0700

    chore(auth): load Firebase key from local path outside repo

--- server/Firebase/firebase.js
+++ server/Firebase/firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("/Users/cjason/private-keys/firebase-service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

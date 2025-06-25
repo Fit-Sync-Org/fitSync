283fa30cd0eadb43962248b9f0c367a4b6ed1969
Jason Jason <jasyn.j.64@gmail.com>
2025-06-24 17:21:23 -0700

    feat(auth): added Firebase login sync and manual register endpoint-backend

--- server/server.js
+++ server/server.js
@@ -7,12 +7,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173", // your frontend URL
+    credentials: true,
+  }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('FitSync server is running');
 });
+
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
 
 app.listen(port, () => {
     console.log(`FitSync server is running on port ${port}`);
